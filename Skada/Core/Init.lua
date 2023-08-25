13b197db1dd32eb35936fb227a9f4ea13ba225ee
Kader Bouyakoub <bkader@mail.com>
2023-08-26 00:50:45 +0100

    small SV fix & cleaning
    - Some profiles cause "Bar.lua" lua error, this was fixed by correcting checks at lines 149 and 154.
    - Renamed "GetSpellLink" to "SpellLink" because the first line is a global, even if it isn't really affected.
    - Small code cleaning (unused lines).

--- Skada/Core/Init.lua
+++ Skada/Core/Init.lua
@@ -1475,6 +1475,7 @@ do
 	-- spell icon and name to speed up things
 	local SpellInfo = Private.SpellInfo
 	ns.spellnames = setmetatable({}, {
+		__mode = "kv",
 		__index = function(t, spellid)
 			local name, _, icon = SpellInfo(spellid)
 			if name then
@@ -1488,6 +1489,7 @@ do
 		end
 	})
 	ns.spellicons = setmetatable({}, {
+		__mode = "kv",
 		__index = function(t, spellid)
 			local name, _, icon = SpellInfo(spellid)
 			if name then
