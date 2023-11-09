98c1e3d1ec6907e665e1335ce55ef77ad03a4a21
Kader Bouyakoub <bkader@mail.com>
2023-11-10 00:43:22 +0100

    UnitFullName instead of UnitName

--- Skada/Core/Functions.lua
+++ Skada/Core/Functions.lua
@@ -2270,8 +2270,7 @@ end
 -- first hit check
 
 do
-	local UnitExists, UnitName = UnitExists, UnitName
-	local SpellLink = Private.SpellLink or GetSpellLink
+	local UnitExists, SpellLink = UnitExists, Private.SpellLink or GetSpellLink
 	local IsPet, uformat = Private.IsPet, Private.uformat
 	local ignored_spells = Skada.ignored_spells.firsthit
 	local firsthit_fmt = {"%s (%s)", "%s (\124c%s%s\124r)", "\124c%s%s\124r", "\124c%s%s\124r (%s)"}
@@ -2293,7 +2292,7 @@ do
 				end
 
 				target_table = target_table or TempTable()
-				target_table:insert(uformat("%s > %s", UnitName(unit), target))
+				target_table:insert(uformat("%s > %s", UnitFullName(unit), target))
 			end
 		end
 
